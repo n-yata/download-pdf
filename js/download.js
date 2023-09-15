0b216d58c3ac4e43f751effebf40666be1d251ef
n-yata <yata1214@gmail.com>
2023-09-16 08:59:28 +0900

    別タブで開いてダウンロード

--- js/download.js
+++ js/download.js
@@ -1,29 +1,20 @@
-function winOpen() {
-  fetchPdf();
-  // window.open("about:blank", "_self").close();
-  // win.close();
-  // let child = window.open( "" , "_blank");
-  // child.close();
-}
+const filePath = "assets/sample.pdf"; // ファイルへのパス
+const downloadFileName = "filename.pdf"; // ダウンロード時のファイル名
+const downloadLinkText = "Download PDF"; // ダウンロードリンクのテキスト
 
-function fetchPdf() {
-  let filePath = "assets/sample.pdf"; // ファイルへのパス
-  let downloadFileName = "filename.pdf"; // ダウンロード時のファイル名
-  let downloadLinkText = "Download PDF"; // ダウンロードリンクのテキスト
-
-  fetch(filePath)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.style.display = "none";
-      a.href = url;
-      a.download = downloadFileName;
-      a.textContent = downloadLinkText;
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    })
-    .catch((error) => console.error("Download failed:", error));
-}
+fetch(filePath)
+  .then((response) => response.blob())
+  .then((blob) => {
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = downloadFileName;
+    a.textContent = downloadLinkText;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+    window.open("about:blank", "_self").close();
+  })
+  .catch((error) => console.error("Download failed:", error));
